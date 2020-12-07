ab907269afdc84ebc0e3aae5a1bd9f5696126917
joielee <2j00923@gmail.com>
2020-12-08 03:46:19 +0900

    added image: tryout building

--- App.js
+++ App.js
@@ -1,13 +1,21 @@
-import { View } from 'native-base';
+import { View, Text } from 'native-base';
 import React from 'react';
 import styled from 'styled-components/native';
+
+const Image = styled.Image`
+    width: 200px;
+    height: 200px;
+    justify-content: center;
+`;
 
 const App = () => {
     return(
         <View>
+            <Image 
+                source={{ uri:'https://thewellesleynews.com/wp-content/uploads/2020/02/Courtesy-of-Looper.jpg' }}
+            />
             <Text>This is overwrite of try out</Text>
         </View>
-
     );
 }
 
